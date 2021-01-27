f91608b2bc5e0948dc35d4058ed2915a2c284893
RhysXia <xrs4433@outlook.com>
2021-01-27 13:45:09 +0800

    docs: demo supports dark mode

--- platforms/docs/src/components/Markdown/index.tsx
+++ platforms/docs/src/components/Markdown/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 // @ts-ignore
 import { MDXProvider } from '@mdx-js/react'
-import 'prismjs/themes/prism.css'
 import DemoBox from '../DemoBox'
+import Wrapper from './Wrapper'
 
 export type MarkdownProps = {
   children: React.ReactNode
 }
 
 const components = {
-  DemoBox
+  DemoBox,
+  wrapper: Wrapper
 }
 
 const Markdown: React.FunctionComponent<MarkdownProps> = (props) => (
-    <MDXProvider components={components}>
-      <div {...props} />
-    </MDXProvider>
-  )
+  <MDXProvider components={components}>
+    <div {...props} />
+  </MDXProvider>
+)
 
 export default Markdown
