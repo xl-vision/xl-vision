02d5d45c197b3e129b8b1eeacbd9309e66c77aaf
Rhys Xia <xrs4433@outlook.com>
2021-02-21 23:30:03 +0800

    fix(styled): fix function styled that could not use component selector

--- packages/react/src/styles/createGlobalStyles.tsx
+++ packages/react/src/styles/createGlobalStyles.tsx
@@ -1,35 +1,15 @@
 import { createGlobalStyles as innerCreateGlobalStyles } from '@xl-vision/styled-engine';
-import {
-  CSSObject,
-  FunctionInterpolation,
-  Interpolation,
-  SimpleInterpolation,
-} from '@xl-vision/styled-engine-types';
+import { CSSObject, FunctionInterpolation, Interpolation } from '@xl-vision/styled-engine-types';
 import { Theme } from '../ThemeProvider/createTheme';
-import defaultTheme from '../ThemeProvider/defaultTheme';
+import { createStyleWithTheme } from './styled';
 
 const createGlobalStyles = <P extends { theme: Theme } = { theme: Theme }>(
   first: TemplateStringsArray | CSSObject | FunctionInterpolation<P>,
   ...styles: Array<Interpolation<P>>
 ) => {
-  const overrideStyle = <Prop extends { theme?: Theme }>(props: Prop) => {
-    const { theme = defaultTheme, ...others } = props;
+  const newArray = [first, ...styles].map(createStyleWithTheme);
 
-    const styleArgs = [first, ...styles];
-
-    const array: Array<SimpleInterpolation> = [];
-
-    styleArgs.forEach((it) => {
-      if (typeof it === 'function') {
-        array.push(it({ theme, ...others }));
-      } else {
-        array.push(it);
-      }
-    });
-
-    return array;
-  };
-
-  return innerCreateGlobalStyles(overrideStyle);
+  // @ts-ignore
+  return innerCreateGlobalStyles(...newArray);
 };
 export default createGlobalStyles;
