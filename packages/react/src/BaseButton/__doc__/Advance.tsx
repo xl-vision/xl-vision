61a3ba5f5bb97557a58aa16fd27738b3f6a9ffb4
xiaruisong <xiaruisong.kuaishou.com>
2021-02-23 14:18:44 +0800

    fix: bug fix

--- packages/react/src/BaseButton/__doc__/Advance.tsx
+++ packages/react/src/BaseButton/__doc__/Advance.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import { BaseButton, styled } from '@xl-vision/react';
 import Breakfast from './breakfast.jpg';
