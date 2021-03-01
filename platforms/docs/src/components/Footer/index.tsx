58e1c658a67de451f1e452eb74d7926101305af5
Rhys Xia <xrs4433@outlook.com>
2021-03-01 22:27:53 +0800

    docs: add footer

--- platforms/docs/src/components/Footer/index.tsx
+++ platforms/docs/src/components/Footer/index.tsx
@@ -1,7 +1,20 @@
+import { styled } from '@xl-vision/react';
 import React from 'react';
 
+const Wrapper = styled('footer')(({ theme }) => {
+  return {
+    padding: '10px 0',
+    borderTop: `1px solid ${theme.color.divider}`,
+    textAlign: 'center',
+  };
+});
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <span>Copyright © 2020-2021 Rhys Xia</span>
+    </Wrapper>
+  );
 };
 
 export default Footer;
