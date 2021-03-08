6f84aefa4c23db5097b3b69f1a8e3d6d423a8d3c
Rhys Xia <xrs4433@outlook.com>
2021-03-09 00:11:51 +0800

    docs: update

--- platforms/docs/src/components/Markdown/inlineCode.tsx
+++ platforms/docs/src/components/Markdown/inlineCode.tsx
@@ -8,7 +8,7 @@ export default styled('code')(
   padding: 0.1em 0.4em;
   font-size: 0.8em;
   font-family: SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace;
-  background-color: ${emphasize(theme.color.background, 0.2)};
+  background-color: ${emphasize(theme.color.background, 0.18)};
   border-radius: 3px;
 `,
 );
