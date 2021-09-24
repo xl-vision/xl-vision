958e4a219aa6268b70e4c6ca7a28887386099ca7
Rhys Xia <xrs4433@outlook.com>
2021-09-24 22:32:09 +0800

    Feat/modal (#23)
    * wip(Modal): popper on Modal can not be focused
    
    * fix(Modal): support internal popper focus
    
    * docs(Modal): add docs
    
    Co-authored-by: xiaruisong <xiaruisong.kuaishou.com>

---
+++ packages/react/src/Modal/__doc__/Shortcut.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/jsx-handler-names */
+import React from 'react';
+import { Button, Modal, Popper, styled } from '@xl-vision/react';
+
+const Body = styled('div')(({ theme }) => {
+  const { color } = theme;
+  return {
+    backgroundColor: color.background.paper,
+    padding: '8px 16px',
+    borderRadius: 4,
+    border: `1px solid ${color.divider}`,
+  };
+});
+
+export default () => {
+  const [visible, setVisible] = React.useState(false);
+
+  return (
+    <>
+      <Button theme='primary' onClick={() => setVisible(true)}>
+        show
+      </Button>
+      <Modal visible={visible} onVisibleChange={setVisible}>
+        <Body>
+          <p>Please click button and try to press key tab</p>
+          <Popper
+            placement='bottom'
+            trigger='click'
+            popup={
+              <Body>
+                <Popper
+                  placement='bottom'
+                  trigger='click'
+                  popup={
+                    <Body>
+                      <span>popper content</span>
+                    </Body>
+                  }
+                >
+                  <Button>Button</Button>
+                </Popper>
+              </Body>
+            }
+          >
+            <Button>Button</Button>
+          </Popper>
+        </Body>
+      </Modal>
+    </>
+  );
+};
