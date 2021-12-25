d1d402f17a8c3401852a93ad7dc33391e9eb7d72
Rhys Xia <xrs4433@outlook.com>
2021-12-25 15:13:14 +0800

    docs: update

--- platforms/docs/sitemap.js
+++ platforms/docs/sitemap.js
@@ -1,7 +1,10 @@
 // use sitemap.js instead of next-sitemap.js.
 // see https://github.com/iamvishnusankar/next-sitemap/issues/61#issuecomment-725999452
+
+const baseUrl = process.env.VERCEL_URL || 'xl-vision.8910.xyz';
+
 module.exports = {
-  siteUrl: 'https://xl-vision.8910.xyz',
+  siteUrl: `https://${baseUrl}`,
   generateRobotsTxt: true,
   changefreq: 'weekly',
 };
