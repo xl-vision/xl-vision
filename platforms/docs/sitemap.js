af4fda001d2aca9847afe2422b8252e4acb40e52
Rhys Xia <xrs4433@outlook.com>
2021-12-25 15:22:47 +0800

    docs: update
    git push

--- platforms/docs/sitemap.js
+++ platforms/docs/sitemap.js
@@ -1,7 +1,11 @@
 // use sitemap.js instead of next-sitemap.js.
 // see https://github.com/iamvishnusankar/next-sitemap/issues/61#issuecomment-725999452
 
-const baseUrl = process.env.VERCEL_URL || 'xl-vision.8910.xyz';
+let baseUrl = process.env.VERCEL_URL || 'xl-vision.8910.xyz';
+
+if (process.env.VERCEL_ENV === 'production') {
+  baseUrl = 'xl-vision.8910.xyz';
+}
 
 module.exports = {
   siteUrl: `https://${baseUrl}`,
