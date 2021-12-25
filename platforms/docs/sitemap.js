29e223e1d19905faa4a0a578497704764b3aa162
Rhys Xia <xrs4433@outlook.com>
2021-12-25 13:17:50 +0800

    docs: automatic generate sitemap.xml and robots.xml

---
+++ platforms/docs/sitemap.js
@@ -0,0 +1,7 @@
+// use sitemap.js instead of next-sitemap.js.
+// see https://github.com/iamvishnusankar/next-sitemap/issues/61#issuecomment-725999452
+module.exports = {
+  siteUrl: 'https://xl-vision.8910.xyz',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+};
