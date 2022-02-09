ca3fe12fe0a51247ab766e8592a17b00f1582a3b
xiaruisong <xiaruisong.kuaishou.com>
2022-02-09 19:15:08 +0800

    wip(Input): working in progress

--- packages/react/src/Input/__doc__/Basic.tsx
+++ packages/react/src/Input/__doc__/Basic.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { Input } from '@xl-vision/react';
 
 const Basic = () => {
-  return <Input placeholder='Basic usage' />;
+  const [value, handleValue] = React.useState('');
+
+  return (
+    <div>
+      <Input placeholder='Basic usage' value={value} onChange={handleValue} maxLength={5} />
+      <p>{value}</p>
+    </div>
+  );
 };
 
 export default Basic;
--- packages/react/src/Input/index.tsx
+++ packages/react/src/Input/index.tsx
@@ -2,33 +2,64 @@ import { env } from '@xl-vision/utils';
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { useConstantFn } from '@xl-vision/hooks';
 import useTheme from '../ThemeProvider/useTheme';
 import { styled } from '../styles';
+import usePropChange from '../hooks/usePropChange';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'type' | 'onChange' | 'value' | 'defaultValue'
+> & {
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  onChange?: (value: string) => void;
+  value?: string;
+  defaultValue?: string;
+  showCount?: boolean;
+  type?:
+    | 'button'
+    | 'checkbox'
+    | 'color'
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'radio'
+    | 'range'
+    | 'reset'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'text'
+    | 'time'
+    | 'url'
+    | 'week';
+};
 
 const displayName = 'Input';
 
-const Root = styled('input', {
+const InputRoot = styled('span', {
   name: displayName,
   slot: 'Root',
 })(({ theme }) => {
-  const { color, shape, typography, mixins } = theme;
+  const { color, shape, transition, typography } = theme;
 
   return {
     ...typography.body1,
-    ...mixins.placeholder(),
     display: 'inline-block',
-    outline: 0,
     borderRadius: shape.borderRadius.md,
     border: `1px solid ${color.divider}`,
-    WebkitAppearance: 'none',
-    color: color.text.primary,
     width: '100%',
-    minWidth: 0,
     padding: '4px 11px',
+    color: color.text.primary,
     backgroundColor: color.background.paper,
-    transition: 'all 0.3s',
+    transition: transition.standard('all'),
 
     '&:hover': {
       borderColor: color.themes.primary.hover,
@@ -36,21 +67,67 @@ const Root = styled('input', {
   };
 });
 
+const InputInner = styled('input', {
+  name: displayName,
+  slot: 'Inner',
+})(({ theme }) => {
+  const { color, typography, mixins } = theme;
+  return {
+    ...typography.body1,
+    ...mixins.placeholder(),
+    display: 'inline-block',
+    minWidth: 0,
+    width: '100%',
+    border: 0,
+    outline: 0,
+    padding: 0,
+    WebkitAppearance: 'none',
+    color: color.text.primary,
+    backgroundColor: color.background.paper,
+  };
+});
+
 const Input: React.FunctionComponent<InputProps> = (props) => {
-  const { className, ...others } = props;
+  const {
+    className,
+    prefix,
+    suffix,
+    defaultValue = '',
+    maxLength,
+    showCount,
+    value: valueProp,
+    onChange,
+    ...others
+  } = props;
 
   const { clsPrefix } = useTheme();
+
+  const [value, handlePropChange] = usePropChange(defaultValue, valueProp, onChange);
+
+  const handleChange = useConstantFn((e: React.ChangeEvent<HTMLInputElement>) => {
+    handlePropChange(e.target.value);
+  });
 
   const rootClassName = `${clsPrefix}-input`;
 
   const rootClasses = clsx(rootClassName, className);
 
-  return <Root {...others} className={rootClasses} />;
+  return (
+    <InputRoot className={rootClasses}>
+      <InputInner
+        {...others}
+        className={`${rootClassName}__inner`}
+        maxLength={maxLength}
+        value={value}
+        onChange={handleChange}
+      />
+    </InputRoot>
+  );
 };
 
 if (!env.isProduction) {
-  Input.displayName = displayName;
-  Input.propTypes = {
+  InputInner.displayName = displayName;
+  InputInner.propTypes = {
     className: PropTypes.string,
   };
 }
