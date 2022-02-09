61de904e5aef32f40f45996ec55051818a91e445
xiaruisong <xiaruisong.kuaishou.com>
2022-02-09 10:09:05 +0800

    chore: update

--- platforms/docs/next.config.js
+++ platforms/docs/next.config.js
@@ -6,6 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   outputFileTracing: false,
   eslint: {
