eb3776b7e457cd95584d4549d431eaacccc771ff
Rhys Xia <xrs4433@outlook.com>
2022-02-12 23:24:22 +0800

    wip(Input): add component Password

---
+++ packages/react/src/Input/Input.tsx
@@ -0,0 +1,418 @@
+import { env } from '@xl-vision/utils';
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { useConstantFn, useForkRef, useUnmount } from '@xl-vision/hooks';
+import { CloseCircleFilled } from '@xl-vision/icons';
+import { CSSObject } from '@xl-vision/styled-engine';
+import useTheme from '../ThemeProvider/useTheme';
+import { styled } from '../styles';
+import usePropChange from '../hooks/usePropChange';
+import { contains } from '../utils/dom';
+import { alpha } from '../utils/color';
+
+export type InputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'type' | 'onChange' | 'value' | 'defaultValue' | 'prefix'
+> & {
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  addonAfter?: React.ReactNode;
+  addonBefore?: React.ReactNode;
+  onChange?: (value: string) => void;
+  value?: string;
+  defaultValue?: string;
+  showCount?: boolean;
+  allowClear?: boolean;
+  type?:
+    | 'button'
+    | 'checkbox'
+    | 'color'
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'radio'
+    | 'range'
+    | 'reset'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'text'
+    | 'time'
+    | 'url'
+    | 'week';
+};
+
+const displayName = 'Input';
+
+const InputRoot = styled('span', {
+  name: displayName,
+  slot: 'Root',
+})(({ theme }) => {
+  const { typography } = theme;
+
+  return {
+    ...typography.body1,
+    width: '100%',
+    display: 'inline-flex',
+  };
+});
+
+const InputAddonBefore = styled('span', {
+  name: displayName,
+  slot: 'AddonBefore',
+})(({ theme }) => {
+  const { color, shape } = theme;
+
+  return {
+    display: 'flex',
+    flex: 'none',
+    alignItems: 'center',
+    backgroundColor: color.emphasize(color.background.paper, 0.05),
+    padding: '0 8px',
+    border: `1px solid ${color.divider}`,
+    borderRightWidth: 0,
+    borderTopLeftRadius: shape.borderRadius.md,
+    borderBottomLeftRadius: shape.borderRadius.md,
+  };
+});
+
+const InputAddonAfter = styled(InputAddonBefore, {
+  name: displayName,
+  slot: 'AddonAfter',
+})(({ theme }) => {
+  const { shape } = theme;
+  return {
+    borderLeftWidth: 0,
+    borderRightWidth: 1,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderTopRightRadius: shape.borderRadius.md,
+    borderBottomRightRadius: shape.borderRadius.md,
+  };
+});
+
+const InputWrapper = styled('span', {
+  name: displayName,
+  slot: 'Wrapper',
+})<{ focused: boolean }>(({ theme, styleProps }) => {
+  const { color, shape, transition } = theme;
+
+  const { focused } = styleProps;
+
+  const focusColor = color.themes.primary.focus;
+
+  const focusedStyle: CSSObject = {
+    borderColor: focusColor,
+    boxShadow: `0 0 0 2px ${alpha(focusColor, 0.2)}`,
+  };
+
+  return {
+    display: 'inline-flex',
+    borderRadius: shape.borderRadius.md,
+    border: `1px solid ${color.divider}`,
+    width: '100%',
+    padding: '4px 11px',
+    color: color.text.primary,
+    backgroundColor: color.background.paper,
+    transition: transition.standard(['borderColor', 'boxShadow']),
+    zIndex: 1,
+
+    '&:not(:first-child)': {
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+    },
+    '&:not(:last-child)': {
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
+    },
+
+    '&:hover': {
+      borderColor: color.themes.primary.hover,
+    },
+
+    ...(focused && focusedStyle),
+  };
+});
+
+const InputInner = styled('input', {
+  name: displayName,
+  slot: 'Inner',
+})(({ theme }) => {
+  const { color, typography, mixins, transition } = theme;
+  return {
+    ...typography.body1,
+    ...mixins.placeholder(),
+    touchAction: 'manipulation',
+    fontVariant: 'tabular-nums',
+    display: 'inline-block',
+    minWidth: 0,
+    width: '100%',
+    border: 0,
+    outline: 0,
+    padding: 0,
+    WebkitAppearance: 'none',
+    color: color.text.primary,
+    backgroundColor: color.background.paper,
+    transition: transition.standard(['borderColor', 'boxShadow']),
+  };
+});
+
+const InputPrefix = styled('span', {
+  name: displayName,
+  slot: 'Prefix',
+})(() => {
+  return {
+    display: 'flex',
+    flex: 'none',
+    alignItems: 'center',
+    marginRight: 4,
+  };
+});
+
+const InputSuffix = styled(InputPrefix, {
+  name: displayName,
+  slot: 'Suffix',
+})(({ theme }) => {
+  const { clsPrefix, color, transition } = theme;
+  return {
+    marginRight: 0,
+    marginLeft: 4,
+    [`.${clsPrefix}-input__suffix--has-suffix`]: {
+      marginRight: 4,
+    },
+    [`.${clsPrefix}-input__suffix-count, .${clsPrefix}-input__suffix-clear`]: {
+      display: 'inline-flex',
+      lineHeight: 1,
+      alignItems: 'center',
+      color: color.text.hint,
+    },
+    [`.${clsPrefix}-input__suffix-clear`]: {
+      cursor: 'pointer',
+      transition: transition.standard('color'),
+      '&:hover': {
+        color: color.text.secondary,
+      },
+    },
+  };
+});
+
+const Input = React.forwardRef<HTMLSpanElement, InputProps>((props, ref) => {
+  const {
+    className,
+    style,
+    prefix,
+    suffix,
+    addonBefore,
+    addonAfter,
+    defaultValue = '',
+    value: valueProp,
+    maxLength,
+    showCount,
+    allowClear,
+    type = 'text',
+    onChange,
+    onBlur,
+    onFocus,
+    ...others
+  } = props;
+
+  const { clsPrefix } = useTheme();
+
+  const [value, handlePropChange] = usePropChange(defaultValue, valueProp, onChange);
+
+  const [focused, setFocused] = React.useState(false);
+
+  const rootRef = React.useRef<HTMLSpanElement>(null);
+
+  const forkRef = useForkRef(rootRef, ref);
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const removePasswordTimerRef = React.useRef<NodeJS.Timeout>();
+
+  const handleChange = useConstantFn((e: React.ChangeEvent<HTMLInputElement>) => {
+    let v = e.target.value;
+    if (typeof v === 'undefined' || v === null) {
+      v = '';
+    }
+
+    handlePropChange(v);
+  });
+
+  const focus = React.useCallback(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleMouseUp = useConstantFn((e: React.MouseEvent) => {
+    const el = rootRef.current;
+    if (!el) {
+      return;
+    }
+    if (contains(el, e.target as Element)) {
+      focus();
+    }
+  });
+
+  const handleFocus = useConstantFn((e: React.FocusEvent<HTMLInputElement>) => {
+    setFocused(true);
+    onFocus?.(e);
+  });
+
+  const handleBlur = useConstantFn((e: React.FocusEvent<HTMLInputElement>) => {
+    setFocused(false);
+    onBlur?.(e);
+  });
+
+  const handleReset = useConstantFn(() => {
+    handlePropChange('');
+  });
+
+  // 将input focus绑定到span上
+  React.useEffect(() => {
+    if (rootRef.current) {
+      rootRef.current.focus = focus;
+    }
+  }, [focus]);
+
+  // 检测是否type=password,并删除value，避免密码泄露
+  React.useEffect(() => {
+    removePasswordTimerRef.current = setTimeout(() => {
+      const input = inputRef.current;
+      if (input && input.getAttribute('type') === 'password' && input.hasAttribute('value')) {
+        input.removeAttribute('value');
+      }
+    });
+  });
+
+  useUnmount(() => {
+    const timer = removePasswordTimerRef.current;
+    if (timer) {
+      clearTimeout(timer);
+    }
+  });
+
+  const rootClassName = `${clsPrefix}-input`;
+
+  const rootClasses = clsx(
+    rootClassName,
+    {
+      [`${rootClassName}--focused`]: focused,
+    },
+    className,
+  );
+
+  let showCountNode: React.ReactNode;
+
+  if (showCount) {
+    const { length } = value;
+    const msg = `${length}${maxLength ? `/${maxLength}` : ''}`;
+
+    const countClasses = clsx(`${rootClassName}__suffix-count`, {
+      [`${rootClassName}__suffix--has-suffix`]: typeof suffix !== 'undefined',
+    });
+
+    showCountNode = <span className={countClasses}>{msg}</span>;
+  }
+
+  let allowClearNode: React.ReactNode;
+
+  if (allowClear && value.length) {
+    const clearClasses = clsx(`${rootClassName}__suffix-clear`, {
+      [`${rootClassName}__suffix--has-suffix`]: showCountNode,
+    });
+
+    allowClearNode = (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+      <span role='button' tabIndex={-1} className={clearClasses} onClick={handleReset}>
+        <CloseCircleFilled />
+      </span>
+    );
+  }
+
+  return (
+    <InputRoot style={style} className={rootClasses} ref={forkRef}>
+      {typeof addonBefore !== 'undefined' && <InputAddonBefore>{addonBefore}</InputAddonBefore>}
+      <InputWrapper
+        className={`${rootClassName}__wrapper`}
+        styleProps={{ focused }}
+        onMouseUp={handleMouseUp}
+      >
+        {typeof prefix !== 'undefined' && (
+          <InputPrefix className={`${rootClassName}__prefix`}>{prefix}</InputPrefix>
+        )}
+        <InputInner
+          {...others}
+          ref={inputRef}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          type={type}
+          className={`${rootClassName}__inner`}
+          maxLength={maxLength}
+          value={value}
+          onChange={handleChange}
+        />
+        {(allowClearNode || showCountNode || typeof suffix !== 'undefined') && (
+          <InputSuffix className={`${rootClassName}__suffix`}>
+            {allowClearNode}
+            {showCountNode}
+            {suffix}
+          </InputSuffix>
+        )}
+      </InputWrapper>
+      {typeof addonAfter !== 'undefined' && <InputAddonAfter>{addonAfter}</InputAddonAfter>}
+    </InputRoot>
+  );
+});
+
+if (!env.isProduction) {
+  Input.displayName = displayName;
+  Input.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    prefix: PropTypes.node,
+    suffix: PropTypes.node,
+    addonBefore: PropTypes.node,
+    addonAfter: PropTypes.node,
+    defaultValue: PropTypes.string,
+    value: PropTypes.string,
+    maxLength: PropTypes.number,
+    showCount: PropTypes.bool,
+    allowClear: PropTypes.bool,
+    type: PropTypes.oneOf([
+      'button',
+      'checkbox',
+      'color',
+      'date',
+      'datetime-local',
+      'email',
+      'file',
+      'hidden',
+      'image',
+      'month',
+      'number',
+      'password',
+      'radio',
+      'range',
+      'reset',
+      'search',
+      'submit',
+      'tel',
+      'text',
+      'time',
+      'url',
+      'week',
+    ]),
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+    onFocus: PropTypes.func,
+  };
+}
+
+export default Input;
