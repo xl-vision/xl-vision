b8489975c1239b9224a98c7efc8dba40f7cc8e7f
Rhys Xia <xrs4433@outlook.com>
2022-02-13 22:43:17 +0800

    docs(Input): update input docs

---
+++ packages/react/src/Input/__test__/index.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import Input from '..';
+
+describe('Input', () => {
+  it.todo('test onChange event', () => {
+    // const handleChange = jest.fn();
+    // const wrapper = mount(<Input onChange={handleChange} />);
+    // wrapper.find('input').simulate('keypress', { key: 'a' });
+    // console.log(wrapper.find('input').debug());
+    // expect(handleChange.mock.calls).toBe('a');
+  });
+});
