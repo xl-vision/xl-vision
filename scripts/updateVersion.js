8cbe24ab28f2b30968d88f3df2ce6153c7af5d09
Rhys Xia <xrs4433@outlook.com>
2022-02-20 13:31:26 +0800

    chore: update

---
+++ scripts/updateVersion.js
@@ -0,0 +1,55 @@
+const semver = require('semver');
+const glob = require('glob');
+const fs = require('fs-extra');
+const path = require('path');
+
+async function run() {
+  const cwd = process.cwd();
+  const basePackagePath = path.resolve(cwd, 'package.json');
+
+  const baseVersion = (await fs.readJSON(basePackagePath)).version;
+
+  const nextVersion = semver.inc(baseVersion, 'prerelease');
+
+  const paths = await findPackageJsons(cwd);
+
+  paths.push(basePackagePath);
+
+  const promises = [];
+
+  paths.forEach((it) => {
+    const p = fs.readJSON(it).then((json) =>
+      fs.writeJSON(
+        it,
+        { ...json, version: nextVersion },
+        {
+          spaces: 2,
+          EOL: '\n',
+        },
+      ),
+    );
+    promises.push(p);
+  });
+
+  await Promise.all(promises);
+}
+
+function findPackageJsons(cwd) {
+  return new Promise((resolve, reject) => {
+    glob(
+      '+(packages|platforms)/*/package.json',
+      {
+        cwd,
+        dot: true,
+      },
+      (err, matches) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(matches);
+      },
+    );
+  });
+}
+
+run();
