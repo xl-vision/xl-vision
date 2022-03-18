20b351bc8221c30956dace60338eee73cd593ffb
Rhys Xia <xrs4433@outlook.com>
2022-03-18 09:39:45 +0800

    test(TextArea): add test cases

--- packages/react/src/TextArea/__test__/index.tsx
+++ packages/react/src/TextArea/__test__/index.tsx
@@ -19,4 +19,57 @@ describe('TextArea', () => {
       expect(wrapper.find(`.xl-textarea--size-${componentSize}`)).not.toBe(null);
     });
   });
+
+  it('test disabled state', () => {
+    const wrapper = mount(<TextArea />);
+
+    wrapper.find('textarea').simulate('focus');
+
+    expect(wrapper.find('.xl-textarea--focused').exists()).toBe(true);
+    expect(wrapper.find('.xl-textarea--disabled').exists()).toBe(false);
+
+    wrapper
+      .setProps({
+        disabled: true,
+      })
+      .update();
+
+    expect(wrapper.find('.xl-textarea--disabled').exists()).toBe(true);
+    expect(wrapper.find('.xl-textarea--focused').exists()).toBe(false);
+  });
+
+  it('test default value', () => {
+    const msg = 'msg';
+
+    const wrapper = mount(<TextArea defaultValue={msg} />);
+
+    const input = wrapper.find('textarea');
+
+    expect(input.getDOMNode<HTMLTextAreaElement>().value).toBe(msg);
+
+    const newMsg = 'new msg';
+
+    input.simulate('change', { target: { value: newMsg } });
+    expect(input.getDOMNode<HTMLTextAreaElement>().value).toBe(newMsg);
+  });
+
+  it('test controlled and uncontrolled state', () => {
+    const msg = 'msg';
+
+    const wrapper = mount(<TextArea />);
+
+    const input = wrapper.find('textarea');
+
+    input.simulate('change', { target: { value: msg } });
+
+    expect(input.getDOMNode<HTMLTextAreaElement>().value).toBe(msg);
+
+    const newMsg = 'new msg';
+
+    wrapper.setProps({ value: newMsg });
+
+    input.simulate('change', { target: { value: msg } });
+
+    expect(input.getDOMNode<HTMLTextAreaElement>().value).toBe(newMsg);
+  });
 });
