7f8d0bdb30fa16c6ec7e7b3557da611c8a0cc1e4
Rhys Xia <xrs4433@outlook.com>
2022-03-24 19:57:33 +0800

    docs: update

--- packages/react/src/utils/ref.ts
+++ packages/react/src/utils/ref.ts
@@ -2,7 +2,7 @@
 import { isMemo } from 'react-is';
 
 export const supportRef = (nodeOrComponent: any): boolean => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   const type = isMemo(nodeOrComponent) ? nodeOrComponent.type.type : nodeOrComponent.type;
 
   // Function component node
