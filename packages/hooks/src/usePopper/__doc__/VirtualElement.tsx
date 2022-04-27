13719d57896277d2b510ea0163d3dd7f9623710e
Rhys Xia <xrs4433@outlook.com>
2022-04-27 20:33:10 +0800

    docs(usePopper): add a demo for virtualElement

---
+++ packages/hooks/src/usePopper/__doc__/VirtualElement.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { usePopper, ReferenceRect } from '@xl-vision/hooks';
+import { styled, Portal } from '@xl-vision/react';
+
+const getContainer = () => document.body;
+
+const Demo = () => {
+  const { x, y, update, reference, popper, mode } = usePopper({
+    mode: 'absolute',
+    placement: 'top',
+  });
+
+  const referenceRectRef = React.useRef<ReferenceRect>({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+
+  React.useEffect(() => {
+    reference({
+      getBoundingClientRect: () => referenceRectRef.current,
+    });
+  }, [reference]);
+
+  const handleMouseMove = React.useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      referenceRectRef.current = {
+        x: e.clientX,
+        y: e.clientY,
+        width: 0,
+        height: 0,
+      };
+      update();
+    },
+    [update],
+  );
+
+  const style = {
+    position: mode,
+    top: 0,
+    left: 0,
+    transform: `translate3d(${Math.round(x)}px, ${Math.round(y)}px, 0)`,
+  };
+
+  return (
+    <DemoRoot onMouseMove={handleMouseMove}>
+      <Portal container={getContainer}>
+        <PortalContent style={style} ref={popper}>
+          Lorem ipsum dolor sit amet.
+        </PortalContent>
+      </Portal>
+    </DemoRoot>
+  );
+};
+
+export default Demo;
+
+const DemoRoot = styled('div')(({ theme }) => {
+  const { color } = theme;
+  return {
+    position: 'relative',
+    height: 200,
+    overflow: 'auto',
+    backgroundColor: color.background.default,
+  };
+});
+
+const PortalContent = styled('div')(({ theme }) => {
+  const { color } = theme;
+  const baseColor = color.mode === 'dark' ? color.modes.light : color.modes.dark;
+
+  return {
+    padding: 8,
+    borderRadius: 4,
+    maxWidth: 100,
+    color: baseColor.text.primary,
+    backgroundColor: color.emphasize(baseColor.background.paper, 0.1),
+  };
+});
