691fad976ba43574cb51b56fb694b9afff6ca68f
Rhys Xia <xrs4433@outlook.com>
2022-06-16 13:22:26 +0800

    wip: work in progress

--- packages/react/src/TransitionGroup/TransitionGroup.tsx
+++ packages/react/src/TransitionGroup/TransitionGroup.tsx
@@ -4,10 +4,11 @@ import { isProduction, warning } from '@xl-vision/utils';
 import {
   CssTransitionClassNameRecord,
   TransitionEndHook,
+  useConstantFn,
   useIsomorphicLayoutEffect,
 } from '@xl-vision/hooks';
 import CssTransition, { CssTransitionProps } from '../CssTransition';
-import diff, { DiffData } from './diff';
+import diff from './diff';
 
 export interface TransitionGroupClassNameRecord
   extends Omit<
@@ -65,17 +66,14 @@ const TransitionGroup: React.FunctionComponent<TransitionGroupProps> = (props) =
     return obj;
   }, [transitionClasses]);
 
-  const prevChildrenRef = React.useRef<Array<React.ReactElement>>();
-
-  const [diffArray, setDiffArray] = React.useState<Array<DiffData>>([]);
+  const [nodes, setNodes] = React.useState<Array<React.ReactElement>>();
 
   const handleExited = React.useCallback(
     (key: React.Key | null) => {
       warning(!key, `<TransitioGroup> must has a key`);
       const hook: TransitionEndHook = (e, transitionOnFirst) => {
-        console.log('====')
         onExited?.(e, transitionOnFirst);
-        prevChildrenRef.current = prevChildrenRef.current?.filter((it) => it.key !== key);
+        setNodes((prev) => prev?.filter((it) => it.key !== key));
       };
 
       return hook;
@@ -83,85 +81,59 @@ const TransitionGroup: React.FunctionComponent<TransitionGroupProps> = (props) =
     [onExited],
   );
 
-  useIsomorphicLayoutEffect(() => {
-    const prevChildren = prevChildrenRef.current;
-    const array = prevChildren
-      ? diff(prevChildren, children)
-      : React.Children.map<DiffData, React.ReactElement>(children, (it) => ({
-          prev: [it],
-          next: [it],
-          same: true,
-        }));
+  const handleChildrenChange = useConstantFn((value: Array<React.ReactElement>) => {
+    const nextChildren = React.Children.map(value, (it) => {
+      return (
+        <CssTransition
+          {...others}
+          key={it.key}
+          in={true}
+          transitionClassName={transitionClassesRecord}
+          mountOnEnter={true}
+          unmountOnExit={true}
+          onExited={handleExited(it.key)}
+        >
+          {it}
+        </CssTransition>
+      );
+    });
 
-    setDiffArray(array);
+    const array = nodes
+      ? diff(nodes, nextChildren)
+      : nextChildren.map((it) => {
+          return {
+            prev: [it],
+            next: [it],
+            same: true,
+          };
+        });
 
-    const nodes: Array<React.ReactElement> = [];
+    const nextNodes: Array<React.ReactElement> = [];
     array.forEach((it) => {
       if (it.same) {
-        nodes.push(...it.next);
+        nextNodes.push(...it.next);
       } else {
-        nodes.push(...it.prev, ...it.next);
+        const prev = it.prev.map((item) => {
+          return React.cloneElement(item, {
+            in: false,
+          });
+        });
+        const next = it.next.map((item) => {
+          return React.cloneElement(item, {
+            in: true,
+            transitionOnFirst: true,
+          });
+        });
+        nextNodes.push(...prev, ...next);
       }
     });
 
-    prevChildrenRef.current = nodes;
-  }, [children]);
-
-  const nodes: Array<React.ReactElement<CssTransitionProps>> = [];
-  diffArray.forEach((it) => {
-    if (it.same) {
-      const array = it.next.map((item) => {
-        return (
-          <CssTransition
-            {...others}
-            key={item.key}
-            in={true}
-            transitionClassName={transitionClassesRecord}
-            mountOnEnter={true}
-            unmountOnExit={true}
-          >
-            {item}
-          </CssTransition>
-        );
-      });
-      nodes.push(...array);
-    } else {
-      const prev = it.prev.map((item) => {
-        return (
-          <CssTransition
-            {...others}
-            key={item.key}
-            in={false}
-            transitionOnFirst={true}
-            mountOnEnter={true}
-            unmountOnExit={true}
-            transitionClassName={transitionClassesRecord}
-            onExited={handleExited(item.key)}
-          >
-            {item}
-          </CssTransition>
-        );
-      });
-
-      const next = it.next.map((item) => {
-        return (
-          <CssTransition
-            {...others}
-            key={item.key}
-            in={true}
-            transitionOnFirst={true}
-            mountOnEnter={true}
-            unmountOnExit={true}
-            transitionClassName={transitionClassesRecord}
-          >
-            {item}
-          </CssTransition>
-        );
-      });
-
-      nodes.push(...prev, ...next);
-    }
+    setNodes(nextNodes);
   });
+
+  useIsomorphicLayoutEffect(() => {
+    handleChildrenChange(children);
+  }, [children, handleChildrenChange]);
 
   return <>{nodes}</>;
 };
