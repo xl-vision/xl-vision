03b7d63041cb1e2b368377375731693c7e9e9cb2
Rhys Xia <xrs4433@outlook.com>
2022-06-23 17:45:58 +0800

    refactor: update

--- packages/hooks/src/usePopper/utils/computeOverflowRect.ts
+++ packages/hooks/src/usePopper/utils/computeOverflowRect.ts
@@ -9,8 +9,7 @@ import {
   oneOf,
 } from '@xl-vision/utils';
 import { MiddlewareParameter, OverflowOptions, OverflowRect, RootBoundary } from '../types';
-import getNodeName from './getNodeName';
-import getParentNode from './getParentNode';
+import getOverflowAncestors from './getOverflowAncestors';
 
 export type Options = OverflowOptions & {
   ctx: MiddlewareParameter;
@@ -125,38 +124,4 @@ const getFiltedOverflowAncestors = (element: Element): Array<Element> => {
   const { offsetParent } = element;
 
   return ancestors.filter((it) => contains(it, offsetParent));
-};
-
-const getOverflowAncestors = (node: Node): Array<Element> => {
-  const list: Array<Element> = [];
-
-  const scrollableAncestor = getNearestOverflowAncestor(node);
-
-  if (scrollableAncestor && scrollableAncestor !== scrollableAncestor.ownerDocument.body) {
-    list.push(scrollableAncestor);
-    list.push(...getOverflowAncestors(scrollableAncestor));
-  }
-
-  return list;
-};
-
-const getNearestOverflowAncestor = (node: Node): Element | null => {
-  const parentNode = getParentNode(node);
-  if (oneOf(['html', 'body', '#document'], getNodeName(parentNode))) {
-    const body = node.ownerDocument?.body;
-    if (body && isOverflowElement(body)) {
-      return body;
-    }
-    return null;
-  }
-  if (isElement(parentNode) && isOverflowElement(parentNode)) {
-    return parentNode;
-  }
-
-  return getNearestOverflowAncestor(parentNode);
-};
-
-const isOverflowElement = (element: Element) => {
-  const { overflow, overflowX, overflowY } = getComputedStyle(element);
-  return /auto|scroll|overlay|hidden/.test(overflow + overflowY + overflowX);
 };
