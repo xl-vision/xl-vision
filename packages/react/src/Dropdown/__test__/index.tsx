d39783db23c486866061970a05cfbc1aeb9143b2
Rhys Xia <xrs4433@outlook.com>
2022-09-25 17:56:17 +0800

    test(Dropdown): add test cases

--- packages/react/src/Dropdown/__test__/index.tsx
+++ packages/react/src/Dropdown/__test__/index.tsx
@@ -1,6 +1,6 @@
-import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
 import { Dropdown } from '@xl-vision/react';
+import { act, fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const menus = (
   <>
@@ -24,19 +24,24 @@ describe('Dropdown', () => {
     jest.useFakeTimers();
   });
 
-  it('test trigger hover', () => {
-    const wrapper = mount(
+  it('Test trigger hover', async () => {
+    const { container } = render(
       <Dropdown menus={menus} id='popup'>
         <button id='btn'>button</button>
       </Dropdown>,
     );
+
     act(() => {
       jest.runAllTimers();
     });
 
     expect(document.querySelector('div#popup')).toBe(null);
 
-    wrapper.find('button#btn').simulate('mouseenter');
+    const user = userEvent.setup({ delay: null });
+
+    const btn = container.querySelector('button#btn')!;
+
+    await user.click(btn);
 
     act(() => {
       jest.runAllTimers();
@@ -57,7 +62,7 @@ describe('Dropdown', () => {
       'none',
     );
 
-    wrapper.find('#btn').simulate('mouseenter');
+    fireEvent.mouseEnter(btn);
 
     act(() => {
       jest.runAllTimers();
@@ -65,12 +70,13 @@ describe('Dropdown', () => {
 
     expect((document.querySelector('#popup')!.firstChild as HTMLDivElement).style.display).toBe('');
 
-    wrapper.find('li#item2').simulate('click');
+    const li = document.querySelector('li#item2')!;
+
+    await user.click(li);
+
     act(() => {
       jest.runAllTimers();
     });
     expect((document.querySelector('#popup')!.firstChild as HTMLDivElement).style.display).toBe('');
-
-    wrapper.unmount();
   });
 });
