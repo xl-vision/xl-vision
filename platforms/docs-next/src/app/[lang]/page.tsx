020801c7cc9bed2fe866cddebd391408c6de65be
Rhys Xia <xrs4433@outlook.com>
2023-05-30 22:33:42 +0800

    docs: handle i18n

---
+++ platforms/docs-next/src/app/[lang]/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>index</h1>
+      <Link href='/test'>link</Link>
+    </div>
+  );
+};
+
+export default Page;
---
+++ platforms/docs-next/src/middleware.ts
@@ -0,0 +1,34 @@
+import Negotiator from 'negotiator';
+import { NextResponse, NextRequest } from 'next/server';
+import { defaultLang, supportedLangs } from './locales';
+
+export const middleware = (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+
+  if (supportedLangs.some((lang) => pathname.startsWith(`/${lang}/`) || pathname === `/${lang}`)) {
+    return NextResponse.next();
+  }
+
+  const headers: Record<string, any> = {};
+
+  request.headers.forEach((v, k) => {
+    headers[k] = v;
+  });
+
+  const negotiator = new Negotiator({ headers });
+
+  const lang = negotiator.language(supportedLangs) || defaultLang;
+
+  return NextResponse.redirect(new URL(`/${lang}/${pathname}`, request.url));
+};
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+    '/((?!favicon\\.svg))',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
