9702bc53c9ec847cdda8b59d5db2573d6a86285c
Rhys Xia <xrs4433@outlook.com>
2025-01-07 14:45:02 +0800

    docs: fix some demo bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,9 @@
-import unicorn from "eslint-plugin-unicorn";
-import _import from "eslint-plugin-import";
-import { fixupPluginRules, fixupConfigRules } from "@eslint/compat";
+import confusingBrowserGlobals from 'confusing-browser-globals';
 import globals from "globals";
-import babelParser from "@babel/eslint-parser";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import babelParser from "@babel/eslint-parser"
+import airbnb from 'eslint-config-airbnb'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
+export default [airbnb, ,{
     ignores: [
         "**/legacy",
         "**/modern",
@@ -31,308 +16,255 @@ export default [{
         "packages/icons/third",
         "packages/icons/scripts/template",
     ],
-}, ...compat.extends(
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:react/jsx-runtime",
-    "plugin:prettier/recommended",
-    "prettier",
-), {
-    plugins: {
-        unicorn,
-        import: (_import),
-    },
-
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-            ...globals.browser,
-        },
-
-        parser: babelParser,
+      globals:  {
+        ...globals.es2016,
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest
+      },
+      parser: babelParser,
+      parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "commonjs",
-
-        parserOptions: {
-            requireConfigFile: false,
-
-            babelOptions: {
-                plugins: [],
-            },
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: [],
         },
+      },
     },
-
-    settings: {
-        react: {
-            version: "detect",
-        },
-
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"],
-        },
-
-        "import/resolver": {
-            typescript: {
-                alwaysTryTypes: true,
-                project: ["packages/*/tsconfig.json", "platforms/*/tsconfig.json"],
-            },
-        },
+  settings: {
+    react: {
+      version: 'detect',
     },
-
-    rules: {
-        "import/no-extraneous-dependencies": ["error", {
-            devDependencies: ["scripts/**", "./.*.js"],
-        }],
-
-        "import/no-unresolved": "error",
-
-        "import/order": ["error", {
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
-            },
-
-            groups: [["builtin", "external"], "internal"],
-        }],
-
-        "no-console": ["error", {
-            allow: ["warn", "error"],
-        }],
-
-        "nonblock-statement-body-position": "error",
-        "no-plusplus": "off",
-        "no-param-reassign": "off",
-
-        "no-restricted-globals": [
-            "error",
-            "addEventListener",
-            "blur",
-            "close",
-            "closed",
-            "confirm",
-            "defaultStatus",
-            "defaultstatus",
-            "event",
-            "external",
-            "find",
-            "focus",
-            "frameElement",
-            "frames",
-            "history",
-            "innerHeight",
-            "innerWidth",
-            "length",
-            "location",
-            "locationbar",
-            "menubar",
-            "moveBy",
-            "moveTo",
-            "name",
-            "onblur",
-            "onerror",
-            "onfocus",
-            "onload",
-            "onresize",
-            "onunload",
-            "open",
-            "opener",
-            "opera",
-            "outerHeight",
-            "outerWidth",
-            "pageXOffset",
-            "pageYOffset",
-            "parent",
-            "print",
-            "removeEventListener",
-            "resizeBy",
-            "resizeTo",
-            "screen",
-            "screenLeft",
-            "screenTop",
-            "screenX",
-            "screenY",
-            "scroll",
-            "scrollbars",
-            "scrollBy",
-            "scrollTo",
-            "scrollX",
-            "scrollY",
-            "self",
-            "status",
-            "statusbar",
-            "stop",
-            "toolbar",
-            "top",
-        ],
-
-        "no-underscore-dangle": "off",
-        "no-nested-ternary": "off",
-        "no-use-before-define": "off",
-        "no-multi-assign": "off",
-        "no-continue": "off",
-
-        "consistent-return": ["off", {
-            treatUndefinedAsUnspecified: true,
-        }],
-
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-
-        "react-hooks/exhaustive-deps": ["error", {
-            additionalHooks: "(useIsomorphicLayoutEffect|useEnhancedMemo)",
-        }],
-
-        "react/jsx-handler-names": ["error", {
-            eventHandlerPrefix: "handle",
-            eventHandlerPropPrefix: "on",
-            checkLocalVariables: true,
-        }],
-
-        "react/function-component-definition": ["error", {
-            namedComponents: "arrow-function",
-        }],
-
-        "react/jsx-no-useless-fragment": ["error", {
-            allowExpressions: true,
-        }],
-
-        "react/forbid-prop-types": "error",
-        "react/jsx-boolean-value": ["error", "always"],
-        "react/display-name": "error",
-        "react/jsx-props-no-spreading": "off",
-        "react/require-default-props": "off",
-
-        "react/jsx-sort-props": ["error", {
-            ignoreCase: true,
-            callbacksLast: true,
-        }],
-
-        "react/sort-prop-types": ["error", {
-            ignoreCase: true,
-            requiredFirst: true,
-            sortShapeProp: true,
-            callbacksLast: true,
-        }],
-
-        "prefer-destructuring": ["error", {
-            VariableDeclarator: {
-                object: true,
-                array: false,
-            },
-
-            AssignmentExpression: {
-                array: false,
-                object: false,
-            },
-        }],
-
-        "unicorn/filename-case": ["error", {
-            cases: {
-                camelCase: true,
-                pascalCase: true,
-            },
-        }],
-
-        "unicorn/better-regex": "error",
-        "unicorn/expiring-todo-comments": "error",
-        "unicorn/consistent-function-scoping": "error",
-        "unicorn/import-index": "error",
-        "unicorn/prefer-query-selector": "off",
-        "unicorn/no-abusive-eslint-disable": "error",
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-)).map(config => ({
-    ...config,
-    files: ["**/*.ts", "**/*.tsx"],
-})), {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['packages/*/tsconfig.json', 'platforms/*/tsconfig.json'],
+      },
     },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
-    },
-
-    rules: {
-        "no-unused-vars": "off",
-        "no-unused-expressions": "off",
-        "no-shadow": "off",
-
-        "react/jsx-filename-extension": ["error", {
-            extensions: [".jsx", ".tsx"],
-        }],
-
-        "import/extensions": ["error", "ignorePackages", {
-            js: "never",
-            mjs: "never",
-            jsx: "never",
-            ts: "never",
-            tsx: "never",
-        }],
-
-        "import/no-cycle": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-
-        "@typescript-eslint/array-type": ["error", {
-            default: "generic",
-        }],
-
-        "@typescript-eslint/no-unused-expressions": "error",
-        "@typescript-eslint/no-shadow": "error",
-
-        "@typescript-eslint/ban-types": ["error", {
-            extendDefaults: true,
-
-            types: {
-                "{}": false,
-            },
-        }],
-
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/no-empty-function": "error",
-    },
-}, {
-    files: [
-        "test/utils/**",
-        "test/__test__/**",
-        "packages/**/__test__/**",
-        "packages/**/__doc__/**",
+  },
+ 
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['unicorn', 'import'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        // packageDir: __dirname,
+        devDependencies: ['scripts/**', './.*.js'],
+      },
     ],
-
-    rules: {
-        "react/jsx-handler-names": "off",
-        "react/display-name": "off",
-        "react/button-has-type": "off",
-        "import/no-unresolved": "off",
-        "import/no-extraneous-dependencies": "off",
-        "import/no-named-as-default": "off",
-        "unicorn/consistent-function-scoping": "off",
-        "import/no-relative-packages": "off",
-        "react/forbid-prop-types": "off",
-        "jsx-a11y/click-events-have-key-events": "off",
-        "jsx-a11y/no-static-element-interactions": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/no-misused-promises": "off",
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
+          caseInsensitive: true /* ignore case. Options: [true, false] */,
+        },
+        groups: [['builtin', 'external'], 'internal'],
+      },
+    ],
+    // Strict, airbnb is using warn; allow warn and error for dev environments
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'nonblock-statement-body-position': 'error',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-globals': ['error'].concat(confusingBrowserGlobals),
+    'no-underscore-dangle': 'off',
+    'no-nested-ternary': 'off',
+    'no-use-before-define': 'off',
+    'no-multi-assign': 'off',
+    'no-continue': 'off',
+    'consistent-return': [
+      'off',
+      {
+        treatUndefinedAsUnspecified: true,
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': [
+      'error',
+      {
+        // custom hooks
+        additionalHooks: '(useIsomorphicLayoutEffect|useEnhancedMemo)',
+      },
+    ],
+    'react/jsx-handler-names': [
+      'error',
+      {
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+        checkLocalVariables: true,
+      },
+    ],
+    'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'react/forbid-prop-types': 'error',
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/display-name': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        ignoreCase: true,
+        callbacksLast: true,
+      },
+    ],
+    'react/sort-prop-types': [
+      'error',
+      {
+        ignoreCase: true,
+        requiredFirst: true,
+        sortShapeProp: true,
+        callbacksLast: true,
+      },
+    ],
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: { object: true, array: false },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+    ],
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+        ignore: [/^.*\.config\.js$/, /^en-US\.ts$/, /^zh-CN\.ts$/],
+      },
+    ],
+    'unicorn/better-regex': 'error',
+    'unicorn/expiring-todo-comments': 'error',
+    'unicorn/consistent-function-scoping': 'error',
+    'unicorn/import-index': 'error',
+    // forbid passing object as default value to props of function component
+    // 'unicorn/no-object-as-default-parameter': 'error',
+    'unicorn/prefer-query-selector': 'off',
+    'unicorn/no-abusive-eslint-disable': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+      ],
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'no-unused-vars': 'off',
+        'no-unused-expressions': 'off',
+        'no-shadow': 'off',
+        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            mjs: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'import/no-cycle': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/array-type': [
+          'error',
+          {
+            default: 'generic',
+          },
+        ],
+        // '@typescript-eslint/member-ordering': [
+        //   'error',
+        //   {
+        //     default: {
+        //       memberTypes: ['signature', 'constructor', 'field', 'method'],
+        //       optionalityOrder: 'required-first',
+        //       order: 'alphabetically-case-insensitive',
+        //     },
+        //   },
+        // ],
+        '@typescript-eslint/no-unused-expressions': 'error',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: true,
+            types: {
+              '{}': false,
+            },
+          },
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-empty-function': 'error',
+      },
     },
-}, {
-    files: ["packages/**/__doc__/**"],
-
-    rules: {
-        "no-console": "off",
+    // {
+    //   files: ['packages/*/src/**/*.ts', 'packages/*/src/**/*.tsx'],
+    //   rules: {
+    //     // 'import/no-unresolved': [
+    //     //   'error',
+    //     //   {
+    //     //     ignore: ['^react', '^react-dom'],
+    //     //   },
+    //     // ],
+    //   },
+    // },
+    {
+      files: [
+        'test/utils/**',
+        'test/__test__/**',
+        'packages/**/__test__/**',
+        'packages/**/__doc__/**',
+      ],
+      rules: {
+        'react/jsx-handler-names': 'off',
+        'react/display-name': 'off',
+        'react/button-has-type': 'off',
+        'import/no-unresolved': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-named-as-default': 'off',
+        'unicorn/consistent-function-scoping': 'off',
+        'import/no-relative-packages': 'off',
+        'react/forbid-prop-types': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+      },
     },
+    {
+      files: ['packages/**/__doc__/**'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
 }];
